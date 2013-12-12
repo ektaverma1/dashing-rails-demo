3d7c8e3180b238b42892e65958316094e28d7cf4
ekta-verma <Ekta_Verma@external.mckinsey.com>
2013-12-12 12:39:30 -0800

    my new dashboard :D

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DashingRailsDemo::Application.config.secret_key_base = '02d1a4b404aae67c6d54f3dc3479e6278910ad31a0287a7e9a0d5b6730044518d41ad04f23e7254b25a182b6173858d9dc11e96f48720d7903e22a087ce18cc1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DashingRailsDemo::Application.config.session_store :cookie_store, key: '_dashing-rails-demo_session'
